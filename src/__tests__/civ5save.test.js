cf70c951a4117ce6fd2b3df828fc0bdea5668962
bmaupin <bmaupin@users.noreply.github.com>
2017-07-12 13:49:06 -0400

    Add initial code and tests

---
+++ src/__tests__/civ5save.test.js
@@ -0,0 +1,28 @@
+import Civ5Save from "../civ5save";
+
+// TODO: Replace this with a relative path if possible
+//const noOptionsSavegamePath = "file://./src/__tests__/resources/victory-all-advanced-all.Civ5Save";
+const noOptionsSavegamePath = "file:///home/user/Dropbox/Projects/javascript-civ5save/src/__tests__/resources/victory-all-advanced-all.Civ5Save";
+// const noOptionsSavegamePath = "file:///home/user/Dropbox/Projects/javascript-civ5save/src/__tests__/resources/test.txt";
+// const noOptionsSavegamePath = "file:///home/user/Dropbox/Projects/javascript-civ5save/src/__tests__/resources/1.0.0.17 (Crash).Civ5Save";
+
+function getFileBlob(url) {
+  return new Promise(function (resolve, reject) {
+    let xhr = new global.XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.responseType = "blob";
+    xhr.addEventListener("load", function() {
+      resolve(xhr.response);
+    });
+    xhr.addEventListener("error", function() {
+      reject(xhr.statusText);
+    });
+    xhr.send();
+  });
+};
+
+test("Get game version", async () => {
+  let fileBlob = await getFileBlob(noOptionsSavegamePath);
+  let savegame = await Civ5Save.fromFile(fileBlob);
+  console.log(savegame.gameVersion);
+});
---
+++ src/civ5save.js
@@ -0,0 +1,72 @@
+const repoUrl = "https://github.com/bmaupin/civ5save";
+
+export default class Civ5Save {
+  constructor(saveData) {
+    this.saveData = saveData;
+
+    this.saveData.getVariableLengthString = function (byteOffset) {
+      let stringLength = this.getStringLength(byteOffset);
+      return this.getAsciiString(byteOffset + 4, stringLength);
+    }
+
+    this.saveData.getStringLength = function (byteOffset) {
+      return saveData.getInt32(byteOffset, true);
+    }
+
+    this.saveData.getAsciiString = function (byteOffset, byteLength) {
+      let string = "";
+      for (let byte = byteOffset; byte < byteOffset + byteLength; byte++) {
+        string += String.fromCharCode(this.getInt8(byte));
+      }
+      return string;
+    }
+  }
+
+  // Use a static factory to instantiate the object since it relies on data that needs to be fetched asynchronously
+  // https://stackoverflow.com/a/24686979/399105
+  static async fromFile(saveFile) {
+    let saveData = await Civ5Save.loadData(saveFile);
+    let civ5save = new Civ5Save(saveData);
+    civ5save.verifyFileSignature();
+    civ5save.verifySaveGameVersion();
+    return civ5save;
+  }
+
+  static loadData(saveFile) {
+    return new Promise(function (resolve, reject) {
+      let reader = new FileReader();
+
+      reader.onload = function () {
+        // Use a DataView for the savegame data since the ArrayBuffer returned by reader.result can't be used to
+        // manipulate the data. A typed array such as Int8Array wouldn't be ideal either since the data contains types
+        // of variable lengths
+        resolve(new DataView(reader.result));
+      }
+      reader.onerror = function () {
+        reject(reader.error);
+      }
+
+      reader.readAsArrayBuffer(saveFile);
+    });
+  }
+
+  verifyFileSignature() {
+    if (this.saveData.getAsciiString(0, 4) !== "CIV5") {
+      throw new Error("File signature does not match. Is this a Civ 5 savegame?");
+    }
+  }
+
+  verifySaveGameVersion() {
+    if (this.saveGameVersion !== 8) {
+      throw new Error(`Savegame version ${this.saveGameVersion} unsupported. Please file an issue at ${repoUrl}.`);
+    }
+  }
+
+  get saveGameVersion() {
+    return this.saveData.getInt32(4, true);
+  }
+
+  get gameVersion() {
+    return this.saveData.getVariableLengthString(8);
+  }
+}
