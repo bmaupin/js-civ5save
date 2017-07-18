823d770b107cdf8cfd875453d16003608d0ad817
bmaupin <bmaupin@users.noreply.github.com>
2017-07-18 14:22:13 -0400

    Speed up tests by instantiating savegames only once

--- src/__tests__/civ5save.test.js
+++ src/__tests__/civ5save.test.js
@@ -5,6 +5,9 @@ const path = require('path');
 const TEST_SAVEGAME_V100 = path.join(__dirname, "resources", "1.0.0.17 (test).Civ5Save");
 const TEST_SAVEGAME_V103 = path.join(__dirname, "resources", "1.0.3.279.victory-all-advanced-all.Civ5Save");
 
+let savegame100;
+let savegame103;
+
 function getFileBlob(url) {
   return new Promise(function (resolve, reject) {
     let xhr = new XMLHttpRequest();
@@ -20,38 +23,29 @@ function getFileBlob(url) {
   });
 };
 
-test("Get game build (v1.0.0)", async () => {
+beforeAll(async () => {
   let fileBlob = await getFileBlob(TEST_SAVEGAME_V100);
-  let savegame = await Civ5Save.fromFile(fileBlob);
-  expect(savegame.gameBuild).toBe("201080");
+  savegame100 = await Civ5Save.fromFile(fileBlob);
+  return savegame100;
 });
 
-test("Get game build (v1.0.3)", async () => {
+beforeAll(async () => {
   let fileBlob = await getFileBlob(TEST_SAVEGAME_V103);
-  let savegame = await Civ5Save.fromFile(fileBlob);
-  expect(savegame.gameBuild).toBe("403694");
+  savegame103 = await Civ5Save.fromFile(fileBlob);
+  return savegame103;
 });
 
-test("Get game version (v1.0.0)", async () => {
-  let fileBlob = await getFileBlob(TEST_SAVEGAME_V100);
-  let savegame = await Civ5Save.fromFile(fileBlob);
-  expect(savegame.gameVersion).not.toBeDefined();
+test("Get game build", () => {
+  expect(savegame100.gameBuild).toBe("201080");
+  expect(savegame103.gameBuild).toBe("403694");
 });
 
-test("Get game version (v1.0.3)", async () => {
-  let fileBlob = await getFileBlob(TEST_SAVEGAME_V103);
-  let savegame = await Civ5Save.fromFile(fileBlob);
-  expect(savegame.gameVersion).toBe("1.0.3.279(130961)");
+test("Get game version", () => {
+  expect(savegame100.gameVersion).not.toBeDefined();
+  expect(savegame103.gameVersion).toBe("1.0.3.279(130961)");
 });
 
-test("Get max turns (v1.0.0)", async () => {
-  let fileBlob = await getFileBlob(TEST_SAVEGAME_V100);
-  let savegame = await Civ5Save.fromFile(fileBlob);
-  expect(savegame.maxTurns).toBe(500);
-});
-
-test("Get max turns (v1.0.3)", async () => {
-  let fileBlob = await getFileBlob(TEST_SAVEGAME_V103);
-  let savegame = await Civ5Save.fromFile(fileBlob);
-  expect(savegame.maxTurns).toBe(330);
+test("Get max turns", () => {
+  expect(savegame100.maxTurns).toBe(500);
+  expect(savegame103.maxTurns).toBe(330);
 });
