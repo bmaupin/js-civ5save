3656b4a68b8b275105cf5d03e18e8c23a6d288cc
bmaupin <bmaupin@users.noreply.github.com>
2017-07-20 14:19:37 -0400

    Minify using webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
+const BabiliPlugin = require('babili-webpack-plugin');
 var path = require('path');
 
 module.exports = {
   entry: './src/civ5save.js',
   output: {
-    filename: 'civ5save.js',
+    filename: 'civ5save.min.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new BabiliPlugin()
+  ]
 };
