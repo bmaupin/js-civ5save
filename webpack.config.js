fe0656a63a0fdcaf5cfc2a7328d4f92875c7bd71
bmaupin <bmaupin@users.noreply.github.com>
2017-07-24 09:24:20 -0400

    Build as a library so it can be imported

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ module.exports = {
   entry: './src/civ5save.js',
   output: {
     filename: 'civ5save.min.js',
+    // library and libraryTarget are necessary so this can be imported as a module
+    library: 'Civ5Save',
+    libraryTarget: 'umd',
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
