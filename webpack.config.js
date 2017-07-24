9abd59513b51c6f6f1b786341da8e3e49011ca74
bmaupin <bmaupin@users.noreply.github.com>
2017-07-24 09:47:15 -0400

    Build minified and unminified versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const BabiliPlugin = require('babili-webpack-plugin');
 var path = require('path');
 
+var env = process.env.NODE_ENV;
+
 module.exports = {
   entry: './src/civ5save.js',
   output: {
-    filename: 'civ5save.min.js',
+    filename: env === 'production' ? 'civ5save.min.js' : 'civ5save.js',
     // library and libraryTarget are necessary so this can be imported as a module
     library: 'Civ5Save',
     libraryTarget: 'umd',
     path: path.resolve(__dirname, 'dist')
   },
-  plugins: [
+  plugins: env === 'production' ? [
     new BabiliPlugin()
-  ]
+  ] : []
 };
