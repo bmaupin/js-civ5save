b050b5f8c271e95c6f7bc373148922e1ca9f9a10
bmaupin <bmaupin@users.noreply.github.com>
2017-08-06 15:56:26 -0400

    Use babel loader instead of babili so we can use ES stage 2 features

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
-const BabiliPlugin = require('babili-webpack-plugin');
 var path = require('path');
 
 var env = process.env.NODE_ENV;
 
 module.exports = {
   entry: './src/civ5save.js',
-  module: env === 'production' ? {
+  module: {
     rules: [
       {
         test: /\.js$/,
-        use: {
-          loader: 'eslint-loader'
-        }
+        use: env === 'production' ? [
+          'babel-loader',
+          'eslint-loader'
+        ] : [
+          'babel-loader'
+        ]
       }
     ]
-  } : {},
+  },
   output: {
     filename: env === 'production' ? 'civ5save.min.js' : 'civ5save.js',
     // library and libraryTarget are necessary so this can be imported as a module
@@ -22,7 +24,4 @@ module.exports = {
     libraryTarget: 'umd',
     path: path.resolve(__dirname, 'dist')
   },
-  plugins: env === 'production' ? [
-    new BabiliPlugin()
-  ] : [],
 };
