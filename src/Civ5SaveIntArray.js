2dbdf73764f9467f0fcbc27d6bbf075ba0eb2413
bmaupin <bmaupin@users.noreply.github.com>
2017-09-05 15:38:09 -0400

    Refactor int array to be closer to string array in case we ever merge them into one class. Bump to v0.0.14

--- src/Civ5SaveIntArray.js
+++ src/Civ5SaveIntArray.js
@@ -3,16 +3,16 @@ import Civ5SaveIntProperty from './Civ5SaveIntProperty';
 export default class Civ5SaveIntArray {
   constructor(byteOffset, items, saveData) {
     this.byteOffset = byteOffset;
-    this.length = items * 4;
     this._array = new Array();
 
-    for (let currentByteOffset = this.byteOffset;
-      currentByteOffset < this.byteOffset + this.length;
-      currentByteOffset += 4) {
+    let currentByteOffset = this.byteOffset;
+    for (let i = 0; i < items; i++) {
       let arrayItem = new Civ5SaveIntProperty(currentByteOffset, 4, saveData);
+      currentByteOffset += arrayItem.length;
       this._array.push(arrayItem.getValue(saveData));
     }
 
+    this.length = currentByteOffset - this.byteOffset;
     Object.freeze(this._array);
   }
 
