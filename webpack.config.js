6312b8f6de8fa2ce1dc89b44963af5228b938c3b
bmaupin <bmaupin@users.noreply.github.com>
2020-01-10 15:00:24 -0500

    Upgrade to webpack v4

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,20 @@
-const env = process.env.NODE_ENV;
 const path = require('path');
 
-module.exports = {
+const config = {
   entry: './src/Civ5Save.js',
-  module: {
-    rules: [
-      {
-        // Fix TypeError: (0 , _typeof3.default) is not a function (https://github.com/webpack/webpack/issues/1694)
-        exclude: /node_modules/,
-        test: /\.js$/,
-        use: [
-          { loader: 'eslint-loader' }
-        ]
-      }
-    ]
-  },
   output: {
-    filename: env === 'production' ? 'civ5save.min.js' : 'civ5save.js',
+    filename: 'civ5save.js',
     // library and libraryTarget are necessary so this can be imported as a module
     library: 'Civ5Save',
     libraryTarget: 'umd',
     path: path.resolve(__dirname, 'dist')
   },
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'production') {
+    config.output.filename = 'civ5save.min.js';
+  }
+
+  return config;
 };
